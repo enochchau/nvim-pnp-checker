c19d69c69e9ff1385ec851f06c46f447f24e7ce1
ec965 <22567131+ec965@users.noreply.github.com>
2022-04-24 05:32:45 -0700

    only check for pnp.cjs

--- lua/nvim-pnp-checker/init.lua
+++ lua/nvim-pnp-checker/init.lua
@@ -1,32 +1,32 @@
 ---@param path string file path
 ---@return boolean
 local function file_exists(path)
-  local f = io.open(path, "r")
-  if f ~= nil then
-    io.close(f)
-    return true
-  else
-    return false
-  end
+	local f = io.open(path, "r")
+	if f ~= nil then
+		io.close(f)
+		return true
+	else
+		return false
+	end
 end
 
 local function check_for_pnp()
-  local lsp_roots = vim.lsp.buf.list_workspace_folders()
-  local has_pnp = true
-  for _, root in ipairs(lsp_roots) do
-    local pnp_path = root .. "/.pnp.cjs"
-    local nm_path = root .. "/node_modules"
-    has_pnp = has_pnp and file_exists(pnp_path) and not file_exists(nm_path)
-  end
-  return has_pnp
+	local lsp_roots = vim.lsp.buf.list_workspace_folders()
+	for _, root in ipairs(lsp_roots) do
+		local pnp_path = root .. "/.pnp.cjs"
+		if file_exists(pnp_path) then
+			return true
+		end
+	end
+	return false
 end
 
 local function get_pnp_cmd()
-  local default_cmd = require('lspconfig.server_configurations.eslint').default_config.cmd
-  return { "yarn", "exec", unpack(default_cmd) };
+	local default_cmd = require("lspconfig.server_configurations.eslint").default_config.cmd
+	return { "yarn", "exec", unpack(default_cmd) }
 end
 
 return {
-  check_for_pnp = check_for_pnp,
-  get_pnp_cmd = get_pnp_cmd
+	check_for_pnp = check_for_pnp,
+	get_pnp_cmd = get_pnp_cmd,
 }
