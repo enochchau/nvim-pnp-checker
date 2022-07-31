04b9ae43ba4ebbde96db7d4ff1ac8bf2cf589cf7
ec965 <22567131+ec965@users.noreply.github.com>
2022-07-31 01:06:25 -0700

    return pnp path

--- lua/nvim-pnp-checker/init.lua
+++ lua/nvim-pnp-checker/init.lua
@@ -17,7 +17,7 @@ local function check_for_pnp()
 	while dir ~= "/" do
 		local pnp_path = dir .. "/.pnp.cjs"
 		if file_exists(pnp_path) then
-			return true
+			return pnp_path
 		end
 
 		dir = vim.fn.fnamemodify(dir, ":h")
@@ -26,9 +26,9 @@ local function check_for_pnp()
 	return false
 end
 
-local function get_pnp_cmd()
+local function get_pnp_cmd(pnp_path)
 	local default_cmd = require("lspconfig.server_configurations.eslint").default_config.cmd
-	return { "yarn", "exec", unpack(default_cmd) }
+	return { 'NODE_OPTIONS="--require ' .. pnp_path .. '"', unpack(default_cmd) }
 end
 
 return {
